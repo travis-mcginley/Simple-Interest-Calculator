552ed32ee85ca451f43b0782a427ea82e8e4d577
travis-mcginley <travis.o.mcginley@gmail.com>
2022-05-05 13:51:44 -0400

    improved functionality of calculator

--- script.js
+++ script.js
@@ -7,26 +7,32 @@ let compute = () => {
     let year        = new Date().getFullYear() + parseInt(years);
     
     // If the amount field is empty, prompt the user to input a value
+    let result_str = ""
     if (principal.value == "") {
-        alert("Please input an amount.");
+        result_str = "You forgot to input a value &#128517;";
         principal.focus();
-        return;
+
+    } else if (principal.value <= 0) {
+        result_str = "Try putting a number greater than zero &#129300;";
+    
+    } else {
+
+        // Calculate the interest using the given params
+        interest = principal.value * years * rate / 100;
+        
+        // Build result string
+        result_str += "If you deposit $";
+        result_str += principal.value;
+        result_str += " today, at an interest rate of ";
+        result_str += rate;
+        result_str += "%, you will receive an amout of $";
+        result_str += interest;
+        result_str += " in the year ";
+        result_str += year;
+        result_str += " &#128526;";
     }
-
-    // Calculate the interest using the given params
-    interest = principal.value * years * rate / 100;
     
-    // Build result string
-    let result_str = "If you deposit $";
-    result_str += principal.value;
-    result_str += ", at an interest rate of ";
-    result_str += rate;
-    result_str += "%, you will receive an amout of $";
-    result_str += interest;
-    result_str += " in the year ";
-    result_str += year;
-    
-
+    document.getElementById("result").innerHTML = result_str;
 }
 
 // Function used to update the value shown next to the rate slider
